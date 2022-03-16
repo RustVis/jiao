381289ad910216e19d52af7f56e4aab03772abc0
Xu Shaohua <shaohua@biofan.org>
2022-03-16 15:24:35 +0800

    Add animation mod

---
+++ src/animation/mod.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+pub mod animation_group;
+pub mod parallel_animation_group;
+pub mod pause_animation;
+pub mod property_animation;
+pub mod sequential_animation_group;
