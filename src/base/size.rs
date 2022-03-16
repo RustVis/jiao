539d886d0526582d677edc1235099e78ab6d9c88
Xu Shaohua <shaohua@biofan.org>
2022-03-16 15:26:24 +0800

    base: Add size and margins

--- src/base/size.rs
+++ src/base/size.rs
@@ -1,10 +1,3 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
-
-pub mod event_dispatcher;
-pub mod margins;
-pub mod point;
-pub mod rect;
-pub mod size;
-pub mod timer;
