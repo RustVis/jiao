943e5aba1ea7451197a518705f445c8c200ee3a7
Xu Shaohua <shaohua@biofan.org>
2022-03-16 14:54:06 +0800

    Add image mod

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -1,10 +1,3 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
-
-pub mod animation;
-pub mod base;
-pub mod image;
-pub mod kernel;
-pub mod painting;
-pub mod util;
