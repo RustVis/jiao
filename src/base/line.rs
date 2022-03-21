d902bf082ee3e678462d1ab5abab5cd862e9d6c8
Xu Shaohua <shaohua@biofan.org>
2022-03-21 13:44:21 +0800

    LineF: Add from_polar()

--- src/base/line.rs
+++ src/base/line.rs
@@ -2,6 +2,7 @@
 // Use of this source is governed by General Public License that can be found
 // in the LICENSE file.
 
+use core::f64::consts::PI;
 use serde::{Deserialize, Serialize};
 
 use super::point::{Point, PointF};
@@ -183,6 +184,17 @@ impl LineF {
         Self { p1, p2 }
     }
 
+    /// Returns a LineF with the given length and angle.
+    ///
+    /// The first point of the line will be on the origin.
+    ///
+    /// Positive values for the angles mean counter-clockwise while negative values
+    /// mean the clockwise direction. Zero degrees is at the 3 o'clock position.
+    pub fn from_polar(length: f64, angle: f64) -> Self {
+        let angle_r = angle * 2.0 * PI / 360.0;
+        LineF::from(0.0, 0.0, angle_r.cos() * length, -angle_r.sin() * length)
+    }
+
     /// Returns the line's start point.
     pub fn p1(&self) -> PointF {
         self.p1
@@ -213,6 +225,27 @@ impl LineF {
         self.p2.y()
     }
 
+    /// Returns the angle of the line in degrees.
+    ///
+    /// The return value will be in the range of values from 0.0 up to but not including 360.0.
+    /// The angles are measured counter-clockwise from a point on the x-axis
+    /// to the right of the origin (x > 0).
+    pub fn angle(&self) -> f64 {
+        unimplemented!()
+    }
+
+    /// Returns the angle (in degrees) from this line to the given line,
+    /// taking the direction of the lines into account.
+    ///
+    /// If the lines do not intersect within their range, it is the intersection point
+    /// of the extended lines that serves as origin.
+    ///
+    /// The returned value represents the number of degrees you need to add to this line
+    /// to make it have the same angle as the given line, going counter-clockwise.
+    pub fn angle_to(&self, line: &LineF) -> f64 {
+        unimplemented!()
+    }
+
     /// Returns the center point of this line.
     ///
     /// This is equivalent to (p1() + p2()) / 2, except it will never overflow.
@@ -233,10 +266,35 @@ impl LineF {
         self.p2.y() - self.p1.y()
     }
 
+    /// Returns a value indicating whether or not this line intersects with the given line.
+    ///
+    /// The actual intersection point is extracted to `intersection_point` (if the pointer is valid).
+    /// If the lines are parallel, the intersection point is undefined.
+    pub fn intersects() {
+        unimplemented!()
+    }
+
     /// Returns true if the line does not have distinct start and end points;
     /// otherwise returns false.
     pub fn is_null(&self) -> bool {
         self.p1 == self.p2
+    }
+
+    /// Returns the length of the line.
+    pub fn length(&self) -> f64 {
+        unimplemented!()
+    }
+
+    /// Returns a line that is perpendicular to this line with the same starting point and length.
+    pub fn normal_vector(&self) -> Self {
+        unimplemented!()
+    }
+
+    /// Returns the point at the parameterized position specified by `t`.
+    ///
+    /// The function returns the line's start point if t = 0, and its end point if t = 1.
+    pub fn point_at(&self, t: f64) -> PointF {
+        unimplemented!()
     }
 
     /// Sets the starting point of this line to `p1`.
@@ -249,6 +307,27 @@ impl LineF {
         self.p2 = p2;
     }
 
+    /// Sets the angle of the line to the given angle (in degrees).
+    ///
+    /// This will change the position of the second point of the line such that the line has the given angle.
+    ///
+    /// Positive values for the angles mean counter-clockwise while negative values
+    /// mean the clockwise direction. Zero degrees is at the 3 o'clock position.
+    pub fn set_angle(&mut self, angle: f64) {
+        unimplemented!()
+    }
+
+    /// Sets the length of the line to the given length.
+    ///
+    /// LineF will move the end point - p2() - of the line to give the line its new length.
+    ///
+    /// A null line will not be rescaled.
+    /// For non-null lines with very short lengths (represented by denormal floating-point values),
+    /// results may be imprecise.
+    pub fn set_length(&mut self, length: f64) {
+        unimplemented!()
+    }
+
     /// Sets this line to the start in `x1`, `y1` and end in `x2`, `y2`.
     pub fn set_line(&mut self, x1: f64, y1: f64, x2: f64, y2: f64) {
         self.p1.set(x1, y1);
@@ -259,6 +338,13 @@ impl LineF {
     pub fn set_points(&mut self, p1: PointF, p2: PointF) {
         self.p1 = p1;
         self.p2 = p2;
+    }
+
+    /// Returns an integer based copy of this line.
+    ///
+    /// Note that the returned line's start and end points are rounded to the nearest integer.
+    pub fn to_line(&self) -> LineF {
+        unimplemented!()
     }
 
     /// Translates this line by the given point (`x`, `y`).
@@ -280,5 +366,27 @@ impl LineF {
     /// Returns this line translated by the given `offset`.
     pub fn translated_point(&self, offset: PointF) -> Self {
         LineF::from_points(self.p1 + offset, self.p2 + offset)
+    }
+
+    /// Returns the unit vector for this line.
+    ///
+    /// i.e a line starting at the same point as this line with a length of 1.0,
+    /// provided the line is non-null.
+    pub fn unit_vector(&self) -> Self {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_polar() {
+        let new_line = LineF::from_polar(8.4, 42.1);
+        assert_eq!(
+            new_line,
+            LineF::from(0.0, 0.0, 6.232597064195178, -5.631583599253912)
+        );
     }
 }
