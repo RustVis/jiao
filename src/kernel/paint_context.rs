bd73997b782eab3ebb78845794a5a7f73c8dc492
Xu Shaohua <shaohua@biofan.org>
2022-04-02 16:19:10 +0800

    Add PaintDevice

--- src/kernel/paint_context.rs
+++ src/kernel/paint_context.rs
@@ -2,14 +2,25 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+use web_sys::HtmlElement;
+
+use super::paint_device::{CanvasPaintDevice, PaintDevice};
 use super::shape_manager::ShapeManager;
 
 pub struct PaintContext {
     shape_manager: ShapeManager,
+    paint_device: Box<dyn PaintDevice>,
 }
 
 impl PaintContext {
-    pub fn new() -> Self {
-        unimplemented!()
+    pub fn from_dom(dom: HtmlElement) -> Self {
+        let shape_manager = ShapeManager::new();
+        let paint_device = CanvasPaintDevice::new(dom);
+        let paint_device = Box::new(paint_device);
+
+        Self {
+            shape_manager,
+            paint_device,
+        }
     }
 }
--- src/kernel/paint_device.rs
+++ src/kernel/paint_device.rs
@@ -1,3 +1,26 @@
 // Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
+
+use wasm_bindgen::JsCast;
+use web_sys::{Document, Element, HtmlCanvasElement, HtmlElement, Window};
+
+pub trait PaintDevice {}
+
+pub struct CanvasPaintDevice {
+    canvas: HtmlCanvasElement,
+    dom: HtmlElement,
+}
+
+impl CanvasPaintDevice {
+    pub fn new(dom: HtmlElement) -> Self {
+        let window: Window = web_sys::window().unwrap();
+        let document: Document = window.document().unwrap();
+        let element: Element = document.create_element("canvas").unwrap();
+        let canvas: HtmlCanvasElement = element.dyn_into::<HtmlCanvasElement>().unwrap();
+        dom.append_child(&canvas).unwrap();
+        Self { canvas, dom }
+    }
+}
+
+impl PaintDevice for CanvasPaintDevice {}
