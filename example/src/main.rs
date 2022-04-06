88ca3ab6bbab93297f47a9d664d0cc681b0340cb
Xu Shaohua <shaohua@biofan.org>
2022-04-06 14:05:56 +0800

    Add example web page

---
+++ example/src/main.rs
@@ -0,0 +1,48 @@
+// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+use yew::{html, Component, Context, Html};
+
+#[derive(Debug)]
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 1 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let _link = ctx.link();
+        html! {
+            <div class="container">
+                <svg width="400" height="210">
+                    <path d="M150 0 L75 200 L225 200 Z" />
+                </svg>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Model>();
+}
