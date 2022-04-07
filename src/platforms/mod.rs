347541a50aaa2ffdbf4694c2d21ca77f0cd88292
Xu Shaohua <shaohua@biofan.org>
2022-04-07 09:43:08 +0800

    Check feature with cfg-if

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -2,14 +2,14 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-#[cfg(feature = "skia")]
-pub mod skia;
-
-#[cfg(feature = "web")]
-pub mod web;
-
-#[cfg(feature = "web")]
-pub type Path = web::Path;
-
-#[cfg(feature = "web")]
-pub type Painter = web::Painter;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "web")] {
+        pub mod web;
+        pub type PaintContext = web::paint_context::PaintContext;
+        pub type PaintDevice = web::paint_device::PaintDevice;
+        pub type Painter = web::painter::Painter;
+        pub type Path = web::painter::Path;
+    } else if #[cfg(feature = "skia")] {
+        pub mod skia;
+    }
+}
--- src/platforms/web/mod.rs
+++ src/platforms/web/mod.rs
@@ -2,10 +2,6 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-mod paint_context;
-mod paint_device;
-mod painter;
-
-pub use paint_context::PaintContext;
-pub use paint_device::PaintDevice;
-pub use painter::{Painter, Path};
+pub mod paint_context;
+pub mod paint_device;
+pub mod painter;
