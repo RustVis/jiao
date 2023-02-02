c8dcba132bb97867bffe2ab21a4fb51fb786b503
Xu Shaohua <shaohua@biofan.org>
2023-02-02 15:05:40 +0800

    examples: Fix cairo example error

--- examples/cairo/src/main.rs
+++ examples/cairo/src/main.rs
@@ -2,6 +2,7 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
+use jiao::error::Error;
 use jiao::kernel::{PaintContextTrait, ShapeManager};
 use jiao::platforms::cairo::{
     ImagePaintDevice, PaintContext, PaintDevice, PdfPaintDevice, SvgPaintDevice,
@@ -25,36 +26,40 @@ fn do_paint(shape_manager: &mut ShapeManager) {
     }
 }
 
-fn draw_png() {
-    let mut paint_device = ImagePaintDevice::new(cairo::Format::ARgb32, 300, 150);
+fn draw_png() -> Result<(), Error> {
+    let mut paint_device = ImagePaintDevice::new(cairo::Format::ARgb32, 300, 150)?;
     let mut paint_ctx = PaintContext::new(PaintDevice::Image(paint_device.clone()));
     let mut shape_manager = paint_ctx.shape_manager();
     do_paint(&mut shape_manager);
     paint_ctx.update();
-    let mut fd = File::create("out.png").unwrap();
-    paint_device.surface().write_to_png(&mut fd).unwrap();
+    let mut fd = File::create("out.png")?;
+    paint_device.surface().write_to_png(&mut fd)?;
+    Ok(())
 }
 
-fn draw_pdf() {
-    let mut paint_device = PdfPaintDevice::new(300.0, 150.0, "out.pdf");
+fn draw_pdf() -> Result<(), Error> {
+    let mut paint_device = PdfPaintDevice::new(300.0, 150.0, "out.pdf")?;
     let mut paint_ctx = PaintContext::new(PaintDevice::Pdf(paint_device.clone()));
     let mut shape_manager = paint_ctx.shape_manager();
     do_paint(&mut shape_manager);
     paint_ctx.update();
     paint_device.surface().finish();
+    Ok(())
 }
 
-fn draw_svg() {
-    let mut paint_device = SvgPaintDevice::new(300.0, 150.0, "out.svg");
+fn draw_svg() -> Result<(), Error> {
+    let mut paint_device = SvgPaintDevice::new(300.0, 150.0, "out.svg")?;
     let mut paint_ctx = PaintContext::new(PaintDevice::Svg(paint_device.clone()));
     let mut shape_manager = paint_ctx.shape_manager();
     do_paint(&mut shape_manager);
     paint_ctx.update();
     paint_device.surface().finish();
+    Ok(())
 }
 
-fn main() {
-    draw_png();
-    draw_pdf();
-    draw_svg();
+fn main() -> Result<(), Error> {
+    draw_png()?;
+    draw_pdf()?;
+    draw_svg()?;
+    Ok(())
 }
