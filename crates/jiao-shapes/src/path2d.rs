495c7485db538b912bf0afa3bd4b96467c43ac9b
Xu Shaohua <shaohua@biofan.org>
2023-02-02 10:52:19 +0800

    shapes: Use GenericPath if no platform specific path is enabled

--- crates/jiao-shapes/src/path2d.rs
+++ crates/jiao-shapes/src/path2d.rs
@@ -2,14 +2,16 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-#[cfg(feature = "cairo")]
-pub type Path2D = jiao_cairo::Path;
-
-#[cfg(feature = "qt")]
-pub type Path2D = jiao_qt::Path;
-
-#[cfg(feature = "skia")]
-pub type Path2D = jiao_skia::Path;
-
-#[cfg(feature = "web")]
-pub type Path2D = jiao_web::Path;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "cairo")] {
+        pub type Path2D = jiao_cairo::Path;
+    } else if #[cfg(feature = "qt")] {
+        pub type Path2D = jiao_qt::Path;
+    } else if #[cfg(feature = "skia")] {
+        pub type Path2D = jiao_skia::Path;
+    } else if #[cfg(feature = "web")] {
+        pub type Path2D = jiao_web::Path;
+    } else {
+        pub type Path2D = jiao::kernel::generic_path::GenericPath;
+    }
+}
