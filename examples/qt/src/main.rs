045c17decaf4e3cbb0c153597d3c829ef97f4539
Xu Shaohua <shaohua@biofan.org>
2023-02-02 11:20:00 +0800

    examples: Fix qt example errors

--- examples/qt/src/main.rs
+++ examples/qt/src/main.rs
@@ -2,19 +2,34 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use jiao::kernel::PaintContextTrait;
-use jiao::platforms::PaintContext;
-use jiao::shapes::LineShape;
+use jiao::kernel::{PaintContextTrait, ShapeManager};
+use jiao_qt::PaintContext;
+use jiao_shapes::LineShape;
 use qt_core::QString;
 use qt_gui::q_image::Format;
 use qt_gui::{QGuiApplication, QImage};
 
+fn do_paint(shape_manager: &mut ShapeManager) {
+    for p in [
+        (10.0, 10.0, 50.0, 10.0),
+        (10.0, 10.0, 10.0, 50.0),
+        (10.0, 50.0, 50.0, 50.0),
+        (50.0, 10.0, 50.0, 50.0),
+        (30.0, 10.0, 30.0, 50.0),
+        (10.0, 30.0, 50.0, 30.0),
+    ]
+    .iter()
+    {
+        let line = LineShape::from_f64(p.0, p.1, p.2, p.3);
+        shape_manager.add(Box::new(line));
+    }
+}
+
 fn draw_png() {
     let image = unsafe { QImage::from_2_int_format(300, 150, Format::FormatRGBA8888) };
     let mut paint_ctx = PaintContext::new();
-    let shape_manager = paint_ctx.shape_manager();
-    let line = LineShape::from_f64(0.0, 0.0, 50.0, 50.0);
-    shape_manager.add(Box::new(line));
+    let mut shape_manager = paint_ctx.shape_manager();
+    do_paint(&mut shape_manager);
     paint_ctx.start(&image);
 
     let ok = unsafe { image.save_q_string(&QString::from_std_str("out.png")) };
