640a0bf39b3d84a47b56eefc8c61214429e57664
Xu Shaohua <shaohua@biofan.org>
2023-02-08 16:20:40 +0800

    examples: Use paint_shapes crate in skia example

--- examples/skia/src/main.rs
+++ examples/skia/src/main.rs
@@ -3,34 +3,18 @@
 // in the LICENSE file.
 
 use jiao::error::{Error, ErrorKind};
-use jiao::kernel::{PaintContextTrait, ShapeManager};
+use jiao::kernel::PaintContextTrait;
 use jiao::platforms::skia::{ImagePaintDevice, PaintContext, PaintDevice};
-use jiao::shapes::LineShape;
+use paint_shapes::paint_shapes;
 use skia_safe::EncodedImageFormat;
 use std::fs::File;
 use std::io::Write;
-
-fn do_paint(shape_manager: &mut ShapeManager) {
-    for p in [
-        (10.0, 10.0, 50.0, 10.0),
-        (10.0, 10.0, 10.0, 50.0),
-        (10.0, 50.0, 50.0, 50.0),
-        (50.0, 10.0, 50.0, 50.0),
-        (30.0, 10.0, 30.0, 50.0),
-        (10.0, 30.0, 50.0, 30.0),
-    ]
-    .iter()
-    {
-        let line = LineShape::from_f64(p.0, p.1, p.2, p.3);
-        shape_manager.add(Box::new(line));
-    }
-}
 
 fn draw_png() -> Result<(), Error> {
     let paint_device = PaintDevice::Image(ImagePaintDevice::new(300, 150));
     let mut paint_ctx = PaintContext::new(paint_device);
     let mut shape_manager = paint_ctx.shape_manager();
-    do_paint(&mut shape_manager);
+    paint_shapes(&mut shape_manager);
     paint_ctx.update();
 
     let paint_device = paint_ctx.paint_device();
