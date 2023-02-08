20bc307a411f402aabccc9b6e8175111b050368b
Xu Shaohua <shaohua@biofan.org>
2023-02-08 16:04:44 +0800

    examples: Remove paintings in qt example

--- examples/qt/src/main.rs
+++ examples/qt/src/main.rs
@@ -2,41 +2,18 @@
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
 
-use jiao::base::{PointF, RectF};
-use jiao::kernel::{PaintContextTrait, ShapeManager};
+use jiao::kernel::PaintContextTrait;
 use jiao::platforms::qt::PaintContext;
-use jiao::shapes::{CircleShape, LineShape, RoundedRectShape};
+use paint_shapes::paint_shapes;
 use qt_core::QString;
 use qt_gui::q_image::Format;
 use qt_gui::{QGuiApplication, QImage};
-
-fn do_paint(shape_manager: &mut ShapeManager) {
-    for p in [
-        (10.0, 10.0, 50.0, 10.0),
-        (10.0, 10.0, 10.0, 50.0),
-        (10.0, 50.0, 50.0, 50.0),
-        (50.0, 10.0, 50.0, 50.0),
-        (30.0, 10.0, 30.0, 50.0),
-        (10.0, 30.0, 50.0, 30.0),
-    ]
-    .iter()
-    {
-        let line = LineShape::from_f64(p.0, p.1, p.2, p.3);
-        shape_manager.add(Box::new(line));
-    }
-
-    let circle = CircleShape::new(PointF::from(75.0, 50.0), 15.0);
-    shape_manager.add(Box::new(circle));
-
-    let rect = RoundedRectShape::new(RectF::from(110.0, 60.0, 50.0, 20.0), 5.0);
-    shape_manager.add(Box::new(rect));
-}
 
 fn draw_png() {
     let image = unsafe { QImage::from_2_int_format(300, 150, Format::FormatRGBA8888) };
     let mut paint_ctx = PaintContext::new();
     let mut shape_manager = paint_ctx.shape_manager();
-    do_paint(&mut shape_manager);
+    paint_shapes(&mut shape_manager);
     paint_ctx.start(&image);
 
     let ok = unsafe { image.save_q_string(&QString::from_std_str("out.png")) };
