20638615f8dc04b580776ac21b18bb3dd8cb8e52
Xu Shaohua <shaohua@biofan.org>
2023-02-12 11:59:22 +0800

    examples: Fix viewport size error in skia example

--- examples/skia/src/main.rs
+++ examples/skia/src/main.rs
@@ -37,7 +37,10 @@ fn draw_png() -> Result<(), Error> {
 }
 
 fn draw_jpg() -> Result<(), Error> {
-    let paint_device = PaintDevice::Image(ImagePaintDevice::new(300, 150));
+    let paint_device = PaintDevice::Image(ImagePaintDevice::new(
+        paint_shapes::CANVAS_WIDTH,
+        paint_shapes::CANVAS_HEIGHT,
+    ));
     let mut paint_ctx = PaintContext::new(paint_device);
     let mut shape_manager = paint_ctx.shape_manager();
     paint_shapes(&mut shape_manager);
