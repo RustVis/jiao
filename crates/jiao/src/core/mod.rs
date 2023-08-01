5ea312be3d757b5d7c9af6204f40b22b54c965e8
Xu Shaohua <shaohua@biofan.org>
2023-08-01 22:50:42 +0800

    jiao: Add path types mod

--- crates/jiao/src/core/mod.rs
+++ crates/jiao/src/core/mod.rs
@@ -1,3 +1,10 @@
 // Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
 // Use of this source is governed by Apache-2.0 License that can be found
 // in the LICENSE file.
+
+//pub mod canvas;
+//pub mod color;
+//pub mod paint;
+//pub mod path;
+
+pub mod path_types;
---
+++ crates/jiao/src/core/path_types.rs
@@ -0,0 +1,77 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum PathFillType {
+    /// Specifies that "inside" is computed by a non-zero sum of signed edge crossings.
+    Winding = 0,
+
+    /// Specifies that "inside" is computed by an odd number of edge crossings.
+    EvenOdd,
+
+    /// Same as Winding, but draws outside of the path, rather than inside.
+    InverseWinding,
+
+    /// Same as EvenOdd, but draws outside of the path, rather than inside.
+    InverseEvenOdd,
+}
+
+impl PathFillType {
+    #[must_use]
+    pub const fn is_event_odd(self) -> bool {
+        let num = self as u8;
+        num & 1 != 0
+    }
+
+    #[must_use]
+    pub const fn is_inverse(self) -> bool {
+        let num = self as u8;
+        num & 2 != 0
+    }
+
+    #[must_use]
+    pub const fn convert_to_non_inverse(self) -> Self {
+        match self {
+            Self::Winding => Self::InverseWinding,
+            Self::EvenOdd => Self::InverseEvenOdd,
+            _ => self,
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum PathDirection {
+    /// clockwise direction for adding closed contours
+    CW,
+    /// counter-clockwise direction for adding closed contours
+    CCW,
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum SkPathSegmentMask {
+    Line = 1 << 0,
+    Quad = 1 << 1,
+    Conic = 1 << 2,
+    Cubic = 1 << 3,
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum PathVerb {
+    /// Iter returns 1 point.
+    Move,
+    /// Iter returns 2 points
+    Line,
+    /// Iter returns 3 points
+    Quad,
+    /// Iter returns 3 points + 1 weight
+    Conic,
+    /// Iter returns 4 points
+    Cubic,
+    /// Iter returns 0 points
+    Close,
+}
