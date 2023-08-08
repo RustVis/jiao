c3eec0cdaa10f9d94a753c26c3d4d0cbbc9577c4
Xu Shaohua <shaohua@biofan.org>
2023-08-08 20:40:02 +0800

    jiao: Remove kernel module

--- crates/jiao/src/kernel/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2022 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
-// Use of this source is governed by Apache-2.0 License that can be found
-// in the LICENSE file.
-
-pub mod cursor;
-pub mod generic_path;
-mod painter;
-
-pub use self::painter::{PainterTrait, PathTrait};
