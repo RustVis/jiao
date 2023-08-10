053dedf09d4c09d37cbd95a3c63c96e2bbe7a163
Xu Shaohua <shaohua@biofan.org>
2023-08-10 17:07:33 +0800

    core: Add Flattenable

---
+++ crates/jiao/src/core/flattenable.rs
@@ -0,0 +1,20 @@
+// Copyright (c) 2023 Xu Shaohua <shaohua@biofan.org>. All rights reserved.
+// Use of this source is governed by Apache-2.0 License that can be found
+// in the LICENSE file.
+
+pub enum Type {
+    ColorFilter,
+    Blender,
+    Drawable,
+    DrawLooper, // deprecated, no longer used internally
+    ImageFilter,
+    MaskFilter,
+    PathEffect,
+    Shader,
+}
+
+/// Flattenable is the trait for objects that need to be flattened
+/// into a data stream for either transport or as part of the key to the font cache.
+pub trait Flattenable {
+    fn get_flattenable_type(&self) -> Type;
+}
